781874065062254cfecac4d10a08c5eb42f34349
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-04-06 17:21:13 +0100

    added list-patients.component

--- web-app/src/app/app.routes.ts
+++ web-app/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './pages/authentication/login/login.component';
 import { PatientFormComponent } from './pages/patients/patient-form/patient-form.component';
 import { CreateQuestionComponent } from './pages/questions/create-question/create-question.component';
 import { ListQuestionsComponent } from './pages/questions/list-questions/list-questions.component';
+import { ListPatientsComponent } from './pages/patients/list-patients/list-patients.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', component: PatientFormComponent },
@@ -14,4 +15,5 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'questions/create', component: CreateQuestionComponent },
   { path: 'questions/list', component: ListQuestionsComponent },
+  { path: 'patients/list', component: ListPatientsComponent },
 ];
---
+++ web-app/src/app/pages/patients/list-patients/list-patients.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-patients',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-patients.component.html',
+  styleUrl: './list-patients.component.scss'
+})
+export class ListPatientsComponent {
+
+}
