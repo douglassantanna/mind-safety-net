44f7a8320fdd174253d59e229000f6f06b9cc1ae
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-04-06 18:48:26 +0100

    added patient-profile component

--- web-app/src/app/pages/patients/list-patients/list-patients.component.ts
+++ web-app/src/app/pages/patients/list-patients/list-patients.component.ts
@@ -22,65 +22,68 @@ export class ListPatientsComponent {
   patients = [
     {
       id: 1,
+      phoneNumber: '123456789',
       fullName: "Patient 1",
       email: "patient1@example.com",
       "priority": 3
     },
     {
       id: 2,
+      phoneNumber: '123456789',
       fullName: "Patient 2",
       email: "patient2@example.com",
       "priority": 2
     },
     {
       id: 3,
+      phoneNumber: '123456789',
       fullName: "Patient 3",
       email: "patient3@example.com",
       "priority": 1
     },
     {
       id: 4,
+      phoneNumber: '123456789',
       fullName: "Patient 4",
       email: "patient4@example.com",
       "priority": 3
     },
     {
       id: 5,
+      phoneNumber: '123456789',
       fullName: "Patient 5",
       email: "patient5@example.com",
       "priority": 2
     },
     {
       id: 6,
+      phoneNumber: '123456789',
       fullName: "Patient 6",
       email: "patient6@example.com",
       "priority": 1
     },
     {
       id: 7,
+      phoneNumber: '123456789',
       fullName: "Patient 7",
       email: "patient7@example.com",
       "priority": 3
     },
     {
       id: 8,
+      phoneNumber: '123456789',
       fullName: "Patient 8",
       email: "patient8@example.com",
       "priority": 2
     },
     {
       id: 9,
+      phoneNumber: '123456789',
       fullName: "Patient 9",
       email: "patient9@example.com",
       "priority": 1
-    },
-    {
-      "id": 10,
-      "fullName": "Patient 10",
-      "email": "patient10@example.com",
-      "priority": 3
     }
   ]
-  displayedColumns = ['fullName', 'email', 'priority', 'actions'];
+  displayedColumns = ['fullName', 'email', 'phoneNumber', 'priority', 'actions'];
   patientDetails(patient: any) { }
 }
