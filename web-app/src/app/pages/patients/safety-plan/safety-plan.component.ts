e67e742f5cd34696f2b75ab1e9f386c31377f6ee
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-04-12 00:06:27 +0100

    wip: safety plan component

--- web-app/src/app/pages/patients/safety-plan/safety-plan.component.ts
+++ web-app/src/app/pages/patients/safety-plan/safety-plan.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-safety-plan',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule
+  ],
   templateUrl: './safety-plan.component.html'
 })
-export class SafetyPlanComponent {
+export class SafetyPlanComponent implements OnInit {
+  private readonly fb = inject(FormBuilder);
+  post = this.fb.group({
+    message: ['', [Validators.required, Validators.maxLength(256)]]
+  });
+  name = 'Douglas'
+  plans = [
+    {
+      title: "My warning signs",
+      message: ""
+    },
+    {
+      title: "My distractions",
+      message: ""
+    },
+    {
+      title: "Professional support",
+      message: ""
+    },
+    {
+      title: "Making my situation safer",
+      message: ""
+    },
+    {
+      title: "My reasons for living",
+      message: ""
+    },
+  ];
+
+  ngOnInit(): void {
+  }
+
+  submitPost() { }
 
 }
